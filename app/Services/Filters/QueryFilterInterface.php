bfa8f108a896e5d34e3db9ff3f3aac666cb4fb46
pzdcprst <serezhaegorov5@gmail.com>
2026-09-09 12:30:38 +0500

    fix: add filter interface

---
+++ app/Services/Filters/QueryFilterInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\Filters;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+interface QueryFilterInterface
+{
+    public function apply(Builder $query, Request $request): Builder;
+}
