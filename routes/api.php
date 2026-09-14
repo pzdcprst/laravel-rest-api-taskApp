6ee22df846464bc125f6f41a6a2d62d9d0e6d2fc
pzdcprst <serezhaegorov5@gmail.com>
2026-09-14 18:00:30 +0500

    fix: pint

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\ProjectController;
 use App\Http\Controllers\Api\V1\StatisticController;
 use App\Http\Controllers\Api\V1\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 
 Route::prefix('v1')->middleware(['throttle:login'])->group(function () {
     Route::post('auth/login', [AuthController::class, 'login']);
