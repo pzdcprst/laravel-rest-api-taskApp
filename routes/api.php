a6cec01a7cb74079e90dc310845a8170a5a6e011
pzdcprst <serezhaegorov5@gmail.com>
2026-09-14 17:58:54 +0500

    fix: Rate limiting

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,12 @@ use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::prefix('v1')->group(function () {
+Route::prefix('v1')->middleware(['throttle:login'])->group(function () {
     Route::post('auth/login', [AuthController::class, 'login']);
     Route::post('auth/register', [AuthController::class, 'register']);
 
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('auth:sanctum')->middleware(['throttle:api'])->group(function () {
         Route::apiResource('projects', ProjectController::class);
 
         Route::patch('tasks/{task}/status', [TaskController::class, 'updateStatus']);
@@ -24,7 +21,7 @@ Route::prefix('v1')->group(function () {
         Route::apiResource('tasks', TaskController::class)->except(['store']);
         Route::get('statistics/tasks', [StatisticController::class, 'statistics']);
 
-        Route::get('auth/logout', [AuthController::class, 'logout']);
+        Route::post('auth/logout', [AuthController::class, 'logout']);
         Route::get('auth/me', [AuthController::class, 'me']);
     });
 });
