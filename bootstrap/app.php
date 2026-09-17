915e4dd6a9768687e31a1e63f2c918fe817ddf80
pzdcprst <serezhaegorov5@gmail.com>
2026-09-17 18:33:21 +0500

    feat: Default exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,10 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use App\Exceptions\TaskExceptions\TaskAlreadyCompletedException;
 use App\Exceptions\TaskExceptions\TaskCannotBeCancelledException;
 use App\Exceptions\TaskExceptions\InvalidTaskStatusTransitionException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -54,4 +58,36 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
+
+        $exceptions->render(function (ModelNotFoundException $exception) {
+            return response()->json([
+                'message' => 'Resource not found',
+            ]);
+        });
+
+        $exceptions->render(function (AuthorizationException $exception) {
+            return response()->json([
+                'message' => $exception->getMessage() ?: "Forbbiden",
+            ], 403);
+        });
+
+        $exceptions->render(function (AuthenticationException $exception) {
+            return response()->json([
+                'message' => "Unauthorized",
+            ], 401);
+        });
+
+        $exceptions->render(function (ValidationException $exception) {
+            return response()->json([
+                'message' => "Validation failed",
+                'errors' => $exception->errors(),
+            ], 422);
+        });
+
+        $exceptions->render(function (Throwable $exception) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], 500);
+        });
+
     })->create();
